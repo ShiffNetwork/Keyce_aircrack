ddd48a59b0914024f6686218992a86e31bf102dc
Antonin <antonin.royer84@gmail.com>
2022-03-17 04:04:34 -0700

    acquisition des réseaux wifi proches

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
+var iwlist = require('wireless-tools/iwlist');
+const path = require('path');
+
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+app.get('/networks', async(req, res) => {
+    const INTERFACE = 'wlx00c0ca970255'
+    iwlist.scan(INTERFACE, function(err, networks) {
+        console.log(networks);
+        res.json(networks)
+    });
+})
